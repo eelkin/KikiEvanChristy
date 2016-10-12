95a8d09649d59baa66dd1e462af67b5df2c4d35a
Evan Elkin <eelkin@mooney202-13.elon.edu>
2016-10-12 15:28:18 -0400

    added programmatic buttons

--- KigiEvanChristy/FirstViewController.swift
+++ KigiEvanChristy/FirstViewController.swift
@@ -12,9 +12,11 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var thePlayersStack: UIStackView!
     
-    var names: [String]
+    var names: [String] = []
     
     var players: [UITextField] = []
+    
+    var buttons: [UIButton] = []
     
     var count = 1
     
@@ -23,6 +25,8 @@ class FirstViewController: UIViewController {
     let spacing: CGFloat = 1
     
     let maxPlayers: CGFloat = 5
+    
+    let playerMax: Int = 5 //because you can't compare an Int to CGFLoat
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,24 +43,43 @@ class FirstViewController: UIViewController {
         
         let textFieldHeight = thePlayersStack.frame.height / maxPlayers
         
-        let player = UITextField(frame: CGRect(x: 0, y: yPos,
-            width: thePlayersStack.frame.width, height: textFieldHeight))
+        if players.count < playerMax {
+            let player = UITextField(frame: CGRect(x: 0, y: yPos,
+                width: thePlayersStack.frame.width, height: textFieldHeight))
+            
+            
+            //player.backgroundColor =
+            player.placeholder = "Player \(count)"
+            //player.font =
+            player.minimumFontSize = 16
+            //player.textColor =
+            player.textAlignment = .center
+            
+            let button = UIButton(frame: CGRect(x:player.frame.width, y: yPos,
+                width: 50, height: textFieldHeight))
+            
+            button.setTitle("Add", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            
+            thePlayersStack.addSubview(player)
+            thePlayersStack.addSubview(button)
         
+            players.append(player)
+            buttons.append(button)
         
-        //player.backgroundColor =
-        player.placeholder = "Player \(count)"
-        //player.font =
-        player.minimumFontSize = 16
-        //player.textColor =
-        player.textAlignment = .center
+            count += 1
+            yPos += textFieldHeight + spacing
+        }
+    }
+    
+    func buttonAction(sender: UIButton!) {
+        sender.backgroundColor = .green
+        print("Hello")
         
-        
-        thePlayersStack.addSubview(player)
-        
-        players.append(player)
-        
-        count += 1
-        yPos += textFieldHeight + spacing
+        /*
+         
+        */
     }
 
 }
--- KigiEvanChristy/ScoreKeeper.swift
+++ KigiEvanChristy/ScoreKeeper.swift
@@ -9,4 +9,4 @@
 import Foundation
 import UIKit
 
-let players
+//let players
